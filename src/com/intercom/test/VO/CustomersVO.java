a780ccb2dd1441df0dd3e7f6c8cc2eb7dc273301
Kannan <kannan@freshdesk.com>
2019-11-11 19:15:52 +0530

    Initial version

---
+++ src/com/intercom/test/VO/CustomersVO.java
@@ -0,0 +1,15 @@
+/**
+ * @packageName : com.intercom.test.VO
+ * @className : CustomersVO.java
+ * @date : 11-Nov-2019
+ * @author : kannans
+ * @version : 1.0
+ */
+
+package com.intercom.test.VO;
+
+public class CustomersVO {
+	
+	
+
+}
